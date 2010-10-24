086b4767f9d2e21e1abd75d457b19834bf015f27
profalbert <cedwani [at] quantentunnel [dot] de>
2010-10-24 16:48:40 +0200

    fix trap-cooldown-grouping

--- Data/Cooldowns.lua
+++ Data/Cooldowns.lua
@@ -122,8 +122,8 @@ local function addGroup(name, ...)
 	end
 end
 
-addGroup(L["Frost traps"], 14311, 13809) -- "Freezing Trap", "Frost Trap"
-addGroup(L["Fire traps"], 27023, 27025, 63668) -- "Immolation Trap", "Explosive Trap", "Black Arrow"
+addGroup(L["Frost traps"], 1499, 80018) -- "Freezing Trap", "Frost Trap"
+addGroup(L["Fire traps"], 13795, 13813, 20733) -- "Immolation Trap", "Explosive Trap", "Black Arrow"
 addGroup(L["Shocks"], 8042, 8050, 8056) -- Earth Shock, Flame Shock, Frost Shock
 addGroup(L["Strikes"], 17364, 73899) -- Stormstrike, Primal Strike
 addGroup(L["Judgements"], 53407, 20271, 53408) -- Judgement of Justice, Judgement of Light, Judgement of Wisdom
