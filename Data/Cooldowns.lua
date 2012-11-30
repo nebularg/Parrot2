656ba6364bba73999878bee40569176c5bfef962
profalbert <cedwani [at] quantentunnel [dot] de>
2012-11-30 17:43:59 +0100

    fix frost-trap-spell-ids

--- Data/Cooldowns.lua
+++ Data/Cooldowns.lua
@@ -122,7 +122,7 @@ local function addGroup(name, ...)
 	end
 end
 
-addGroup(L["Frost traps"], 1499, 67035) -- "Freezing Trap", "Frost Trap"
+addGroup(L["Frost traps"], 1499, 13809) -- "Freezing Trap", "Ice Trap"
 addGroup(L["Fire traps"], 13795, 13813, 20733) -- "Immolation Trap", "Explosive Trap", "Black Arrow"
 addGroup(L["Shocks"], 8042, 8050, 8056) -- Earth Shock, Flame Shock, Frost Shock
 addGroup(L["Strikes"], 17364, 73899) -- Stormstrike, Primal Strike
